e29af5dba6ca05372e9b483bebfd8abb5ff91587
BuchananSEO <68229983+BuchananSEO@users.noreply.github.com>
2022-04-23 10:35:01 -0700

    first commit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,11 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiKey: '328492337dbe5161b740c178aa6028b3',
+  baseUrl: 'https://api.themoviedb.org/3',
+  images: 'http://image.tmdb.org/t/p'
+
 };
 
 /*
